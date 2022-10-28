0e8bf345d1e12f87bd39f2c67016ddf864f4d42e
smiethereens23nas <nicholas23smith@gmail.com>
2022-10-28 18:37:54 -0400

    added about us title

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,9 @@ const About = () => {
   return (
     <div className='about-container'>
 <img className='about-img' src={"/Tom&Lex.png"} alt="about us" />
-<div className='about-text-container'></div>
+<div className='about-text-container'>
+    <h1>About Us</h1>
+</div>
     </div>
   )
 }
