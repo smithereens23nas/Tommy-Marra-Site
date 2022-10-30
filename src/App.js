35f894a98c2dbffc51731f0a87b354dae08caa7b
smiethereens23nas <nicholas23smith@gmail.com>
2022-10-29 20:27:06 -0400

    added api key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 import { Home, Navbar, Products, About } from './components'
 
 const App = () => {
+const [products, setProducts] = useState([]);
+const [cart, setCart] = useState({})
+
+
+// const fetchCart = async () => {
+//   const response = await 
+// }
   return (
     <div>
     <Navbar />
